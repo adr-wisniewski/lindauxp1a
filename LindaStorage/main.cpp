8615fa5f2fab10dee55ff11d0ef2c22d7584cb8d
sirizaak <sirizaak@2a3f17aa-e98d-11de-a5fe-b7516cc0fd57>
2009-12-15 20:09:09 +0000

    Created

---
+++ LindaStorage/main.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   main.cpp
+ * Author: adrian
+ *
+ * Created on 12 grudzień 2009, 22:07
+ */
+
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
