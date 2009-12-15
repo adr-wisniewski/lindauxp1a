5b0dfbdf7b6944d15625e342ff270dfb9beed256
sirizaak <sirizaak@2a3f17aa-e98d-11de-a5fe-b7516cc0fd57>
2009-12-15 20:08:18 +0000

    Created

---
+++ Linda/Exception.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   Exception.h
+ * Author: adrian
+ *
+ * Created on 15 grudzień 2009, 10:23
+ */
+
+#ifndef _EXCEPTION_H
+#define	_EXCEPTION_H
+
+#include <stdexcept>
+#include <string>
+
+namespace Linda
+{
+
+    class Exception : public std::runtime_error
+    {
+    public:
+        Exception(int errno, std::string &prefix);
+    };
+}
+
+#endif	/* _EXCEPTION_H */
+
